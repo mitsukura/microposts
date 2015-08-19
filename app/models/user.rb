acaeb2048ae7d835657d9d26ee0309b7d3e59d78
mitsukura <mitsukura@gmail.com>
2015-08-19 12:38:48 +0000

    reveiw2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,5 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
 
     has_secure_password
-    
-    validates :profile, presence: true, length: { maximum: 200 }
-    validates :location, presence: true, length: { maximum:10}
+
 end
