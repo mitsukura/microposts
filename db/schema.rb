c7cd53689393986534c0309982404d6819aab004
mitsukura <mitsukura@gmail.com>
2015-08-22 13:47:56 +0000

    add posting microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819103023) do
+ActiveRecord::Schema.define(version: 20150819124216) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
