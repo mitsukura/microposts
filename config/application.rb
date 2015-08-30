e7f69c6205eae6024e0b2c635bd6e54e1f7d1892
mitsukura <mitsukura@gmail.com>
2015-08-30 11:15:41 +0000

    add pagenation

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module Microposts
     end
     config.active_record.raise_in_transactional_callbacks = true
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.i18n.default_locale = :ja
   end
 end
